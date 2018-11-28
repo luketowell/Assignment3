eb0528750cf065b8ef1d1ac1ac792b157367b872
Unknown <luke.towell@hotmail.com>
2018-11-28 22:54:30 +0000

    Install the if statement for GPU detection.
    - Install an if statement for detection of GPU or not
    - Added in the indentation for the if statement.

--- Assignment3.cu
+++ Assignment3.cu
@@ -6,14 +6,14 @@ __global__ void exponentialFunction (int dataPoints, float *devX, float *devY)
 {
    float first, second, third;
    int my_i = blockIdx.x*blockDim.x + threadIdx.x;
-
+	
    if (my_i<dataPoints+1){
       first = ((devX[my_i]-2))*((devX[my_i]-2));
       second = (pow((devX[my_i]-6.0),2)/10);
-      third = (1/(pow((double)devX[my_i],2.0)+1));
+      third = (1/(pow((double)devX[my_i],2.0)+1)); 
       devY[my_i] = (expf(-first)+expf(-second)+third);
    }
-
+	
 }
 
 // Serial Code
@@ -35,90 +35,100 @@ void serialFunction (int dataPoints, float *X, float *Y)
 
 int main()
 {
-   int i;
-   float steps = 200;
-   int dataPoints = 1000;
+   int i, numGPU;
+	
+   cudaGetDeviceCount(&numGPU);
+   if (numGPU >= 1) {
 
-   // X and F(x) as Y declaration
-   float *X, *Y;
-   float *devX, *devY;
+      float steps = 200;
+      int dataPoints = 1000;
 
-   //Insert here if statement for if not detected;
+      // X and F(x) as Y declaration
+      float *X, *Y;
+      float *devX, *devY;
+      //Insert here if statement for if not detected;
+  
+      //create cuda timing objects
+      cudaEvent_t startCuda, stopCuda;
+      cudaEventCreate(&startCuda);
+      cudaEventCreate(&stopCuda);
+      // Device memory allocation
+      cudaMalloc(&devX, dataPoints*sizeof(float));
+      cudaMalloc(&devY, dataPoints*sizeof(float));
 
-   //create cuda timing objects
-   cudaEvent_t startCuda, stopCuda;
-   cudaEventCreate(&startCuda);
-   cudaEventCreate(&stopCuda);
+      //Host Memory Allocation
+      X = (float *) malloc(sizeof(float)*dataPoints);
+      Y = (float *) malloc(sizeof(float)*dataPoints);
+	
+      float discretePoint = steps/dataPoints;
 
-   // Device memory allocation
-   cudaMalloc(&devX, dataPoints*sizeof(float));
-   cudaMalloc(&devY, dataPoints*sizeof(float));
 
-   //Host Memory Allocation
-   X = (float *) malloc(sizeof(float)*dataPoints);
-   Y = (float *) malloc(sizeof(float)*dataPoints);
+      //discretise the range to work out X[i]
+      for (i= 0; i < dataPoints+1; i++){
+         X[i] = (discretePoint * i)-100;
+      }
 
-   float discretePoint = steps/dataPoints;
+      // Copy the host contents of X over to device devX
+      cudaMemcpy(devX, X, dataPoints*sizeof(float), cudaMemcpyHostToDevice);	
 
-   //discretise the range to work out X[i]
-   for (i= 0; i < dataPoints+1; i++){
-      X[i] = (discretePoint * i)-100;
-   }
+      // Check for errors after Copying X over to new Device
+      cudaError err = cudaGetLastError();
+      if (err != cudaSuccess) {
+        printf("(1) CUDA RT error: %s \n", cudaGetErrorString(err));
+      }
+  
+      //Work out threads and blocks and print out number of threads and blocks
+      int threads = 8;
+      int blocks = ceil((float)dataPoints/(float)threads);
+      printf("using %d threads on %d blocks \n", threads, blocks);
 
-   // Copy the host contents of X over to device devX
-   cudaMemcpy(devX, X, dataPoints*sizeof(float), cudaMemcpyHostToDevice);       
 
-   // Check for errors after Copying X over to new Device
-   cudaError err = cudaGetLastError();
-   if (err != cudaSuccess) {
-     printf("(1) CUDA RT error: %s \n", cudaGetErrorString(err));
-   }
+      //Start the Cuda Timings
+      cudaEventRecord(startCuda, 0);
+
+      //Call the function kernel
+      exponentialFunction<<<blocks,threads>>> (dataPoints, devX, devY);
+      //Stop the Cuda Timings
+      cudaEventRecord(stopCuda, 0);
    
-   //Work out threads and blocks and print out number of threads and blocks
-   int threads = 32;
-   int blocks = ceil((float)dataPoints/(float)threads);
-   printf("using %d on %d blocks \n", threads, blocks);
+      // check for errors after running Kernel
+      err = cudaGetLastError();
+      if (err != cudaSuccess) {
+        printf("(2) CUDA RT error: %s \n", cudaGetErrorString(err));
+      }
 
-   //Start the Cuda Timings
-   cudaEventRecord(startCuda, 0);
+      // Copy over the Y value from the device to the host
+      cudaMemcpy(Y, devY, dataPoints*sizeof(float), cudaMemcpyDeviceToHost);
+   
+      //Check for errors after copying errors over from device to host.
+      err = cudaGetLastError();
+      if (err != cudaSuccess) {
+        printf("(3) CUDA RT error: %s \n", cudaGetErrorString(err));
+      }
 
-   //Call the function kernel
-   exponentialFunction<<<blocks,threads>>> (dataPoints, devX, devY);
-   //Stop the Cuda Timings
-   cudaEventRecord(stopCuda, 0);
+      //clean up memory
+      cudaFree(devX);
+      cudaFree(devY);	
+  
+      //Work out time
+      float cTime;
+      cudaEventElapsedTime(&cTime, startCuda, stopCuda);
+      printf("Ran on device in: %f microseconds \n", cTime);
+ 
+      //print out the Cuda+OMP result and timing
+      /*for(i=0; i < dataPoints+1; i++)
+      {
+         printf("X = %0.5f \n", X[i]);
+         printf("Y = %0.5f \n", Y[i]);
+      }*/
 
-   // check for errors after running Kernel
-   err = cudaGetLastError();
-   if (err != cudaSuccess) {
-     printf("(2) CUDA RT error: %s \n", cudaGetErrorString(err));
+      //call the serial code:
+      serialFunction(dataPoints, X, Y);
+      printf("%f \n", discretePoint);
    }
-
-   // Copy over the Y value from the device to the host
-   cudaMemcpy(Y, devY, dataPoints*sizeof(float), cudaMemcpyDeviceToHost);
-
-   //Check for errors after copying errors over from device to host.
-   err = cudaGetLastError();
-   if (err != cudaSuccess) {
-     printf("(3) CUDA RT error: %s \n", cudaGetErrorString(err));
-   }
-
-   //clean up memory
-   cudaFree(devX);
-   cudaFree(devY);
-
-   //Work out time
-   float cTime;
-   cudaEventElapsedTime(&cTime, startCuda, stopCuda);
-   printf("Ran on device in: %f microseconds \n", cTime);
-
-   //print out the Cuda+OMP result and timing
-   for(i=0; i < dataPoints+1; i++)
+   else
    {
-      printf("X = %0.5f \n", X[i]);
-      printf("Y = %0.5f \n", Y[i]);
+    //No GPU detected  
+    printf("No GPUs are detected!\n");
    }
-
-   //call the serial code:
-   serialFunction(dataPoints, X, Y);
-   printf("%f \n", discretePoint);
 }
