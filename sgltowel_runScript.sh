ca9672d1ae7ca4bb10694efbb8e01cdefaa7be30
Unknown <luke.towell@hotmail.com>
2018-11-29 22:20:56 +0000

    Rearranged the input variables
    Rearranged the input variables so that they are more related.
    
    Added in the run command to run the cuda code.

--- sgltowel_runScript.sh
+++ sgltowel_runScript.sh
@@ -7,24 +7,24 @@ if [[ $# -ne 4 ]]; then
         exit -1
 fi
 
-EXE=./sgltowel_Code.exe
-OMPTHREADS=$1
-GPUTHREADS=$2
-REPEATS=$3
-ARRAY_LENGTH=$4
-FILE=CUDAfor${ARRAY_LENGTH}on${GPUTHREADS}by${REPEATS}.txt
+EXE=sgltowel_code.exe
+OMPTHREADS=$2
+GPUTHREADS=$3
+REPEATS=$1
+DATAPOINTS=$4
+FILE=CUDAfor${DATAPOINTS}on${GPUTHREADS}by${REPEATS}.txt
 
 #Work out number of blocks
-let GPUBLOCKS=($4+$2-1)/$2
+let GPUBLOCKS=($4+$3-1)/$3
 
 echo best of $REPEATS for executable $EXE
 
-        export OMP_NUM_THREADS=$THREADS
-        echo running ${EXE} with an array length of ${ARRAY_LENGTH}
+        export OMP_NUM_THREADS=$OMPTHREADS
+        echo running ${EXE} with an array length of ${DATAPOINTS}
         echo running on ${GPUTHREADS} threads across ${GPUBLOCKS} blocks.
-        #for k in `seq 1 $REPEATS`; do
-                
-        #done  > $FILE
+        for k in `seq 1 $REPEATS`; do
+            ./${EXE} ${DATAPOINTS} ${GPUTHREADS}            
+        done  > $FILE
         echo outputting data to ${FILE}
 
 echo Finished
