e2c6eaeafee31961690a59ebab30c4263aa0cb97
Unknown <luke.towell@hotmail.com>
2018-11-29 22:13:25 +0000

    Added in open mp
    - Changed the file name of the output file
    - Added in the gnu compiler to compile -fopenmp

--- sgltowel_compileScript.sh
+++ sgltowel_compileScript.sh
@@ -9,10 +9,10 @@ if [[ $# -ne 0 ]]; then
 fi
 
 EXE=sgltowel_code.cu
-OUTPUTFILE=sgltowel-code.exe
+OUTPUTFILE=sgltowel_code.exe
 
 echo running ${COMPILECOMMAND}
 echo load module cuda-8.0
 module load cuda-8.0
-nvcc ./${EXE} -o ./${OUTPUTFILE}
+nvcc -Xcompiler -fopenmp ./${EXE} -o ./${OUTPUTFILE}
 echo $OUTPUTFILE file compiled
