1a8a16f88396d6c1a72baea23c4d4694622e7967
Unknown <luke.towell@hotmail.com>
2018-11-29 08:21:52 +0000

    Run Script
    - First pass on the run script which takes 4 arguments. allowing hopefully for fully customisable code usage.
    
    - Outputs relevant information to the user and logs the output of the file to the file.

---
+++ sgltowel_runScript.sh
@@ -0,0 +1,30 @@
+usage(){
+        echo $0 ./parallelCode.exe numOMPThreads numGPUThreadsPC numRepeats arrayLength 
+}
+
+if [[ $# -ne 4 ]]; then
+        usage
+        exit -1
+fi
+
+EXE=./sgltowel_Code.exe
+OMPTHREADS=$1
+GPUTHREADS=$2
+REPEATS=$3
+ARRAY_LENGTH=$4
+FILE=CUDAfor${ARRAY_LENGTH}on${GPUTHREADS}by${REPEATS}.txt
+
+#Work out number of blocks
+let GPUBLOCKS=($4+$2-1)/$2
+
+echo best of $REPEATS for executable $EXE
+
+        export OMP_NUM_THREADS=$THREADS
+        echo running ${EXE} with an array length of ${ARRAY_LENGTH}
+        echo running on ${GPUTHREADS} threads across ${GPUBLOCKS} blocks.
+        #for k in `seq 1 $REPEATS`; do
+                
+        #done  > $FILE
+        echo outputting data to ${FILE}
+
+echo Finished
