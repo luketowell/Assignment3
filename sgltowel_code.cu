a1ea8d62571bd3d0d6487a80d820f0fd6f78c792
Unknown <luke.towell@hotmail.com>
2018-12-02 17:51:54 +0000

    Loads of changes to the file
    - Final commit of the sgltowel_code.cu file. contains all logic and code that has been completed.

--- sgltowel_code.cu
+++ sgltowel_code.cu
@@ -4,66 +4,126 @@
 #include <stdlib.h>
 
 // Code for the parallel CUDA code
-__global__ void exponentialFunction (int dataPoints, float *devX, float *devY)
+__global__ void exponentialFunction (int dataPoints, float *X, float *Y)
 {
    float first, second, third;
    int my_i = blockIdx.x*blockDim.x + threadIdx.x;
 	
    if (my_i<dataPoints+1){
-      first = ((devX[my_i]-2))*((devX[my_i]-2));
-      second = (pow((devX[my_i]-6.0),2)/10);
-      third = (1/(pow((double)devX[my_i],2.0)+1)); 
-      devY[my_i] = (expf(-first)+expf(-second)+third);
+      first = ((X[my_i]-2))*((X[my_i]-2));
+      second = (pow((X[my_i]-6.0),2)/10);
+      third = (1/(pow((double)X[my_i],2.0)+1)); 
+      Y[my_i] = (expf(-first)+expf(-second)+third);
    }
 	
 }
 
-// Serial Code
-void serialFunction (int dataPoints, float *serialX, float *serialY)
+__global__ void initX (float *X, int dataPoints, float discretePoint, int threads)
 {
-   float first, second, third;
-   int i;
-
-   for(i=0; i < dataPoints+1; i++)
-      {
-         first = ((serialX[i]-2))*((serialX[i]-2));
-         second = (pow((serialX[i]-6),2)/10);
-         third = (1/(pow(serialX[i],2)+1));
-         serialY[i] = (exp(-first)+exp(-second)+third);
-      }
-
-   printf("ran serial code \n");
+    int my_i = blockIdx.x*blockDim.x + threadIdx.x;
+ 
+    if (my_i < dataPoints+1){
+        X[my_i] = (discretePoint * my_i)-100; 
+    }
 }
 
 int main(int argc, char **argv)
-{
+{   
    int i, numGPU;
-   	
+   // Steps between the two outer values e.g. -100 and +100.
+   float steps = 200;
+   // set the amount of dataPoints that you want to discretize the function over
+   int dataPoints = strtol(argv[1], NULL, 10);
+
+   //all serial code
+   float *serialX, *serialY;
+   float serialMaxY, serialFirst, serialSecond, serialThird;
+
+   double serialFunctionStart, serialFunctionEnd, serialStart, serialEnd, serialInitStart, serialInitEnd, serialMaxStart, serialMaxEnd;
+   
+   serialX = (float *) malloc(sizeof(float)*dataPoints);
+   serialY = (float *) malloc(sizeof(float)*dataPoints);
+   //start serial timings
+   serialStart = omp_get_wtime();
+   
+   //work out discrete point again for serial
+   float serialDiscretePoint = steps/dataPoints;
+
+   //discretise the range to work out X[i]
+   serialInitStart = omp_get_wtime();
+
+   for (i= 0; i < dataPoints+1; i++){
+     serialX[i] = (serialDiscretePoint * i)-100;
+   }
+   
+   serialInitEnd = omp_get_wtime();
+
+   //work out F(x) as Y serial code:
+   serialFunctionStart = omp_get_wtime(); 
+   
+   for(i=0; i < dataPoints+1; i++)
+   {
+      serialFirst = ((serialX[i]-2))*((serialX[i]-2));
+      serialSecond = (pow((serialX[i]-6),2)/10);
+      serialThird = (1/(pow(serialX[i],2)+1));
+      serialY[i] = (exp(-serialFirst)+exp(-serialSecond)+serialThird);
+   }
+   serialFunctionEnd = omp_get_wtime();
+   
+   serialMaxStart = omp_get_wtime();
+   //work out max in serial
+   for(i=0; i < dataPoints+1; i++)
+   {
+      if (serialY[i] > serialMaxY){
+          serialMaxY = serialY[i];
+      }
+    }
+   serialMaxEnd = omp_get_wtime();
+
+   //end serial timings
+   serialEnd = omp_get_wtime();
+   
+   //Start cuda code execution
    cudaGetDeviceCount(&numGPU);
    if (numGPU >= 1) {
-      // Steps between the two outer values e.g. -100 and +100.
-      float steps = 200;
-
-      // set the amoutn of dataPoints that you want to discretize the function over
-      int dataPoints = strtol(argv[1], NULL, 10);
-
       //set number of omp threads to be used;
-      omp_set_num_threads(12);
-      int ompthreads = omp_get_max_threads();
-      printf("number of omp threads = %d", ompthreads);
+      int numOMPThreads = strtol(argv[3], NULL, 10); 
+      omp_set_num_threads(numOMPThreads);
  
       // X and F(x) as Y declaration
-      float *X, *Y, *serialX, *serialY;
+      float *X, *Y;
       float *devX, *devY;
-      float maxY, serialMaxY;
+      float maxY;
   
       //create cuda timing objects
-      cudaEvent_t startCuda, stopCuda;
+      cudaEvent_t cudaIStart, cudaIEnd, startCuda, stopCuda;
+      cudaEventCreate(&cudaIStart);
+      cudaEventCreate(&cudaIEnd);
       cudaEventCreate(&startCuda);
       cudaEventCreate(&stopCuda);
+      
+      //declare and work out the number of threads and blocks.
+      int potentialBlocks, maxBlocks = 65535; 
+      int threads = strtol(argv[2], NULL, 10);
+
+      // Ternary statement for working out amout of blocks to be used.
+      potentialBlocks = ceil((float)dataPoints/(float)threads);
+      int blocks = (potentialBlocks<maxBlocks) ? potentialBlocks : maxBlocks ;
+      
+      //statement to ensure enough threads are used.
+      
+      if(blocks==maxBlocks){
+        int minThreads = ceil((float)dataPoints/(float)blocks);
+        threads = minThreads;
+        printf("You have not requested enough threads for the program to execute accurately.\n We are using the minimum number of threads required which for your data size is : %d\n", minThreads);
+      }	
+
+
+      printf("Discretizing the function across %d datapoints using %d threads on %d blocks \n", dataPoints, threads, blocks);
 
       //OMP timing variables
-      double cudaStart, cudaEnd, serialFunctionStart, serialFunctionEnd, serialStart, serialEnd, serialInitStart, serialInitEnd, ompInitStart, ompInitEnd, ompMaxStart, ompMaxEnd, serialMaxStart, serialMaxEnd;
+      double cudaStart, cudaInitEnd,cudaFuncMemStart, cudaFuncMemEnd, cudaEnd;
+      double ompMaxStart, ompMaxEnd;
       
       // Device memory allocation
       cudaMalloc(&devX, dataPoints*sizeof(float));
@@ -72,40 +132,29 @@ int main(int argc, char **argv)
       //Host Memory Allocation
       X = (float *) malloc(sizeof(float)*dataPoints);
       Y = (float *) malloc(sizeof(float)*dataPoints);
-      serialX = (float *) malloc(sizeof(float)*dataPoints);
-      serialY = (float *) malloc(sizeof(float)*dataPoints);
+      
       
       //Start executing
       cudaStart = omp_get_wtime();	
       float discretePoint = steps/dataPoints;
 
+      //init the values of X cuda timings
+      cudaEventRecord(cudaIStart,0);
+      initX<<<blocks, threads>>>(devX, dataPoints, discretePoint, threads);
+      cudaEventRecord(cudaIEnd);
+      cudaEventSynchronize(cudaIEnd);
+      cudaMemcpy(X, devX, dataPoints*sizeof(float), cudaMemcpyDeviceToHost);
+      cudaInitEnd = omp_get_wtime();	
 
-      //discretise the range to work out X[i]
-      ompInitStart = omp_get_wtime();
-      #pragma omp parallel default(none) shared(dataPoints, X, discretePoint)
-      {
-          #pragma omp for private(i) schedule(static, 1)
-          for (i= 0; i < (dataPoints+1); i++){
-             X[i] = (discretePoint * i)-100;
-          }
-      }
-      ompInitEnd = omp_get_wtime();
-
+      cudaFuncMemStart = omp_get_wtime();
       // Copy the host contents of X over to device devX
       cudaMemcpy(devX, X, dataPoints*sizeof(float), cudaMemcpyHostToDevice);	
-
+    
       // Check for errors after Copying X over to new Device
       cudaError err = cudaGetLastError();
       if (err != cudaSuccess) {
         printf("(1) CUDA RT error: %s \n", cudaGetErrorString(err));
       }
-  
-      //Work out threads and blocks and print out number of threads and blocks
-      int threads = strtol(argv[2], NULL, 10);
-      int blocks = ceil((float)dataPoints/(float)threads);
-
-      printf("using %d threads on %d blocks \n", threads, blocks);
-
 
       //Start the Cuda Timings
       cudaEventRecord(startCuda, 0);
@@ -113,8 +162,8 @@ int main(int argc, char **argv)
       //Call the function kernel
       exponentialFunction<<<blocks,threads>>> (dataPoints, devX, devY);
       //Stop the Cuda Timings
-      cudaEventRecord(stopCuda, 0);
-   
+      cudaEventRecord(stopCuda);
+      cudaEventSynchronize(stopCuda);
       // check for errors after running Kernel
       err = cudaGetLastError();
       if (err != cudaSuccess) {
@@ -123,18 +172,22 @@ int main(int argc, char **argv)
 
       // Copy over the Y value from the device to the host
       cudaMemcpy(Y, devY, dataPoints*sizeof(float), cudaMemcpyDeviceToHost);
-   
+      cudaFuncMemEnd=omp_get_wtime();
       //Check for errors after copying errors over from device to host.
       err = cudaGetLastError();
       if (err != cudaSuccess) {
         printf("(3) CUDA RT error: %s \n", cudaGetErrorString(err));
       }
 
-      //clean up memory
+      //clean up device memory
       cudaFree(devX);
       cudaFree(devY);
 	
       //Work out time
+      //CUDA initialisation timing
+      float iTime;
+      cudaEventElapsedTime(&iTime, cudaIStart, cudaIEnd);
+      //CUDA function timing
       float cTime;
       cudaEventElapsedTime(&cTime, startCuda, stopCuda);
       
@@ -151,55 +204,27 @@ int main(int argc, char **argv)
       //end of cuda+omp implementation
       cudaEnd = omp_get_wtime();
 
-      //start serial timings
-      serialStart = omp_get_wtime();
-      
-      //work out discrete point again for serial
-      float serialDiscretePoint = steps/dataPoints;
-
-
-      //discretise the range to work out X[i]
-      serialInitStart = omp_get_wtime();
-
-      for (i= 0; i < dataPoints+1; i++){
-        serialX[i] = (serialDiscretePoint * i)-100;
-      }
-      
-      serialInitEnd = omp_get_wtime();
-
-      //call the serial code:
-      serialFunctionStart = omp_get_wtime(); 
-      serialFunction(dataPoints,serialX, serialY);
-      serialFunctionEnd = omp_get_wtime();
-      
-      serialMaxStart = omp_get_wtime();
-      //work out max in serial
-      for(i=0; i < dataPoints+1; i++)
-      {
-         if (serialY[i] > serialMaxY){
-             serialMaxY = Y[i];
-         }
-      }
-      serialMaxEnd = omp_get_wtime();
-
-      //end serial timings
-      serialEnd = omp_get_wtime();
-      
       //total timings
-      printf("omp init %0.5f\n", (ompInitEnd - ompInitStart)*1000);
-      printf("cuda function: %0.5f\n", cTime);
-      printf("omp max calc: %0.5f\n", (ompMaxEnd - ompMaxStart)*1000);
-      printf("total cuda Time: %0.5f\n", (cudaEnd - cudaStart)*1000);
-      printf("serial init %0.5f\n", (serialInitEnd - serialInitStart)*1000);
-      printf("serial function: %0.5f\n", (serialFunctionEnd-serialFunctionStart)*1000);
-      printf("serial max calc: %0.5f\n", (serialMaxEnd - serialMaxStart)*1000);
-      printf("all serial: %0.5f \n", (serialEnd - serialStart) * 1000);
-      printf("cuda+omp maxY: %0.8f\n", maxY);
-      printf("serial maxY: %0.8f\n", serialMaxY);
-
+      printf("~~~~~~~~~~~~~~~~~~~~~~~~~ Results of Run ~~~~~~~~~~~~~~~~~~~~~~~\n");
+      printf("|  Program Step  | Kernel + Mem  |    Kernel   |    Serial     |\n");
+      printf("|______________________________________________________________|\n");
+      printf("| Init of X      |  %0.8f  |  %0.8f |  %0.8f |\n", (cudaInitEnd - cudaStart)*1000, iTime, (serialInitEnd - serialInitStart)*1000);
+      printf("| Calculate F(x) |  %0.8f  | %0.8f | %0.8f |\n", (cudaFuncMemEnd - cudaFuncMemStart)*1000, cTime, (serialFunctionEnd-serialFunctionStart)*1000);
+      printf("| Max of F(x)    | ~~~~ N/A ~~~~ | %0.8f |   %0.8f |\n", (ompMaxEnd - ompMaxStart)*1000, (serialMaxEnd - serialMaxStart)*1000);
+      printf("|______________________________________________________________|\n");
+      printf("|                |      Cuda + OMP      |      Serial Code     |\n", (ompMaxEnd - ompMaxStart)*1000, (serialMaxEnd - serialMaxStart)*1000);
+      printf("| Total Time     |     %0.8f     |     %0.8f    |\n", (cudaEnd - cudaStart)*1000, (serialEnd - serialStart) * 1000);
+      printf("| Max Value F(x) |       %0.8f     |        %0.8f    |\n", maxY, serialMaxY); 
+      printf("|______________________________________________________________|\n");
    }
    else
    {
-    printf("No GPUs are detected!\n");
+    printf("serial init %0.5f\n", (serialInitEnd - serialInitStart)*1000);
+    printf("Unfortunately no GPUs are detected!\n");
+    printf("Here are the serial results!\n");
+    printf("serial function: %0.5f\n", (serialFunctionEnd-serialFunctionStart)*1000);
+    printf("serial max calc: %0.5f\n", (serialMaxEnd - serialMaxStart)*1000);
+    printf("all serial: %0.5f \n", (serialEnd - serialStart) * 1000);
+    printf("serial maxY: %0.8f\n", serialMaxY);
    }
 }
