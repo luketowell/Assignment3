c4f2b74f156c14ab00e4ef49195571f7477bba1e
Unknown <luke.towell@hotmail.com>
2018-12-02 17:48:32 +0000

    run script with the repeat functionality
    - Run script with the repeat functionality so you can run many times and output to screen.

---
+++ sgltowel_runScriptwithRepear.sh
@@ -0,0 +1,30 @@
+usage(){
+        echo $0 numOfRepeats numOMPThreads numGPUThreads arrayLength 
+}
+
+if [[ $# -ne 4 ]]; then
+        usage
+        exit -1
+fi
+
+EXE=sgltowel_code.exe
+OMPTHREADS=$2
+GPUTHREADS=$3
+REPEATS=$1
+DATAPOINTS=$4
+FILE=CUDAfor${DATAPOINTS}on${GPUTHREADS}by${REPEATS}.txt
+
+#Work out number of blocks
+let GPUBLOCKS=($4+$3-1)/$3
+
+echo best of $REPEATS for executable $EXE
+
+        export OMP_NUM_THREADS=$OMPTHREADS
+        echo running ${EXE} with an array length of ${DATAPOINTS}
+        echo running on ${GPUTHREADS} threads across ${GPUBLOCKS} blocks.
+        for k in `seq 1 $REPEATS`; do
+            ./${EXE} ${DATAPOINTS} ${GPUTHREADS} ${OMPTHREADS}            
+        done  > $FILE
+        echo outputting data to ${FILE}
+
+echo Finished
