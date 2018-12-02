c1229e191eea11a0e1f2baacf724d1902f017a29
Unknown <luke.towell@hotmail.com>
2018-12-02 17:44:41 +0000

    Final run script which matches requirements
    - removed all of the repeat logic which rproduces many repeats.
    - Hard coded the dataPoints to 3000000
    - removed the output to file so just outputs to screen.

--- sgltowel_runScript.sh
+++ sgltowel_runScript.sh
@@ -1,30 +1,19 @@
 usage(){
-        echo $0 numOfRepeats numOMPThreads numGPUThreadsPC arrayLength 
+        echo $0  numOMPThreads numGPUThreads arrayLength 
 }
 
-if [[ $# -ne 4 ]]; then
+if [[ $# -ne 3 ]]; then
         usage
         exit -1
 fi
 
 EXE=sgltowel_code.exe
-OMPTHREADS=$2
-GPUTHREADS=$3
-REPEATS=$1
-DATAPOINTS=$4
-FILE=CUDAfor${DATAPOINTS}on${GPUTHREADS}by${REPEATS}.txt
+OMPTHREADS=$1
+GPUTHREADS=$2
+DATAPOINTS=30000000
 
-#Work out number of blocks
-let GPUBLOCKS=($4+$3-1)/$3
-
-echo best of $REPEATS for executable $EXE
-
-        export OMP_NUM_THREADS=$OMPTHREADS
-        echo running ${EXE} with an array length of ${DATAPOINTS}
-        echo running on ${GPUTHREADS} threads across ${GPUBLOCKS} blocks.
-        for k in `seq 1 $REPEATS`; do
-            ./${EXE} ${DATAPOINTS} ${GPUTHREADS}            
-        done  > $FILE
-        echo outputting data to ${FILE}
+export OMP_NUM_THREADS=$OMPTHREADS
+echo executing file: ${EXE}
+./${EXE} ${DATAPOINTS} ${GPUTHREADS} ${OMPTHREADS}            
 
 echo Finished
